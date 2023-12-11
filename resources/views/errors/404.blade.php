145c7a5e6896d9f868ab37bde20b0a380870ba69
JD Singh <jagdeepsingh@founderandlightning.com>
2023-12-11 22:10:13 +0530

    php artisan enlightn:  - Add env-file for config helper  - Use non-sync queue  - Enable Opcache in source Dockerfile  - Add a 404 page  - Remove php-exposing configuration from php.ini and use updated Dockerfile instead

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>{{ config('env_vars.APP_NAME') }}</title>
+</head>
+<body class="antialiased">
+    404 not found!
+</body>
+</html>
