6e8080f793faa7cd401f8ce68b7beadcad9a46ee
JD Singh <jagdeepsingh@founderandlightning.com>
2023-12-18 21:34:47 +0530

    Add GitHub OAuth App env-var/placeholder values

--- config/env_vars.php
+++ config/env_vars.php
@@ -3,4 +3,8 @@
 return [
     'APP_NAME' => env('APP_NAME', 'watcher!'),
     'APP_ENV' => env('APP_ENV', 'production'),
+
+    'GITHUB_CLIENT_ID' => env('GITHUB_CLIENT_ID', 'helo.github.com'),
+    'GITHUB_CLIENT_SECRET' => env('GITHUB_CLIENT_SECRET', 'github_client_secret_placeholder'),
+    'GITHUB_CALLBACK' => env('GITHUB_CALLBACK', '/auth/github/callback'),
 ];
