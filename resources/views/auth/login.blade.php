3c0ebde4466b0b15c7a303efb140c4e332a1cbb5
JD Singh <jagdeepsingh@founderandlightning.com>
2023-12-29 16:10:19 +0530

    Add link to register on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,8 @@
 <x-guest-layout>
+    <div class="text-white text-3xl text-center">
+        Login
+    </div>
     <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
-
         <x-validation-errors class="mb-4" />
 
         @if (session('status'))
@@ -32,10 +31,15 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-around mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+                @if (Route::has('register'))
+                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                        {{ __('Register!') }}
                     </a>
                 @endif
 
