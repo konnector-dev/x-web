0efaeef8edb4cf6e989e00bae403e6d99112854d
JD Singh <jagdeepsingh@founderandlightning.com>
2023-12-29 16:11:24 +0530

    Login with GitHub : white logo/text on black bg, rounded border

--- resources/views/components/github-oauth.blade.php
+++ resources/views/components/github-oauth.blade.php
@@ -1,17 +1,17 @@
 <div class="block mt-4">
     <div class="mt-4">
         <a
-            class="w-2/3 mx-auto px-4 py-2 flex items-center justify-center border-2 border-black bg-black/20"
+            class="w-2/3 mx-auto px-4 py-2 flex items-center justify-center rounded-xl border-2 border-black bg-black"
             href="{{ url(route('github.login')) }}">
             <div>
                 <img
-                    src="{{ asset('images/raw-github.svg') }}"
+                    src="{{ asset('images/raw-github-white.svg') }}"
                     alt="GitHub OAuth Login"
                     title="GitHub OAuth Login"
                     class="w-12 h-12"
                 />
             </div>
-            <span class="text-gray-400 ml-2">
+            <span class="text-white ml-2">
                 {{ __('Login with GitHub') }}
             </span>
         </a>
