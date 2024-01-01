ae6525ad9f6ee415fffedd5687ce290ee3968b95
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-01 18:08:56 +0530

    Text color defaults to white for welcome layout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -11,9 +11,9 @@
             </a>
         </div>
         @auth
-            <a href="{{ url(route('dashboard')) }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500">Dashboard</a>
+            <a href="{{ url(route('dashboard')) }}" class="px-6 py-3 rounded-full font-semibold bg-blue-500/50 hover:bg-blue-600/50">Dashboard</a>
         @else
-            <div class="flex items-center text-xl gap-6 text-white leading-6 font-semibold">
+            <div class="flex items-center text-xl gap-6 leading-6 font-semibold">
                 <a href="{{ url(route('register')) }}">Register</a>
                 <a
                     href="{{ url(route('login')) }}"
