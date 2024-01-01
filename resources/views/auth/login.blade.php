6601b566013df12b98e912a29949bd897605b531
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-01 17:49:45 +0530

    Login/Register components use separate OAuth login button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,52 +2,61 @@
     <div class="text-white text-3xl text-center">
         Login
     </div>
-    <x-authentication-card>
-        <x-validation-errors class="mb-4" />
+    <div class="flex flex-col space-y-4">
+        <x-authentication-card>
+            <x-validation-errors class="mb-4"/>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-                {{ session('status') }}
-            </div>
-        @endif
+            @if (session('status'))
+                <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
+                <div>
+                    <x-label for="email" value="{{ __('Email') }}"/>
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                             required autofocus autocomplete="username"/>
+                </div>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+                <div class="mt-4">
+                    <x-label for="password" value="{{ __('Password') }}"/>
+                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                             autocomplete="current-password"/>
+                </div>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+                <div class="block mt-4">
+                    <label for="remember_me" class="flex items-center">
+                        <x-checkbox id="remember_me" name="remember"/>
+                        <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
 
-            <div class="flex items-center justify-around mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-                @if (Route::has('register'))
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
-                        {{ __('Register!') }}
-                    </a>
-                @endif
+                <div class="flex items-center justify-around mt-4">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-gray-800"
+                           href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+                    @if (Route::has('register'))
+                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-gray-800"
+                           href="{{ route('register') }}">
+                            {{ __('Register!') }}
+                        </a>
+                    @endif
 
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-        <x-github-oauth />
-    </x-authentication-card>
+                    <x-button class="ms-4">
+                        {{ __('Log in') }}
+                    </x-button>
+                </div>
+            </form>
+        </x-authentication-card>
+        <div class="text-center">
+            Or
+        </div>
+        <x-github-oauth/>
+    </div>
 </x-guest-layout>
