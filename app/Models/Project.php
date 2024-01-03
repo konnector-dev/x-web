31a0d2eda678126ca384e1a282691f6fc4ceaeb7
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-03 17:34:00 +0530

    Fillable `name`, `description` in `Project` Model

---
+++ app/Models/Project.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Project extends Model
+{
+    use HasFactory;
+    use softDeletes;
+
+    protected $fillable = ['name', 'description'];
+}
