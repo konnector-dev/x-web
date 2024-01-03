0e65021fcffb91fa37dd1058aa195404bbfc35e3
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-03 17:45:53 +0530

    Remove redundant bg and text classes

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,11 +1,11 @@
-<div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
+<div class="p-6 lg:p-8 border rounded-xl border-gray-300">
     <x-application-logo class="block h-12 w-auto" />
 
-    <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+    <h1 class="mt-8 text-2xl font-medium text-white">
         Welcome to Watcher!
     </h1>
 
-    <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
+    <p class="mt-6 text-gray-400 leading-relaxed">
         Watcher watches your websites and APIs for downtime and notifies you, so that you can sleep peacefully.
     </p>
 </div>
