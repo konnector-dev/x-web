21a649ecf50eebf725db590ee3719dd602f4e032
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-05 17:09:50 +0530

    Publish livewire config + Add placeholder/loader

---
+++ resources/views/livewire/placeholders/default.blade.php
@@ -0,0 +1,13 @@
+<div class="w-full animate-pulse flex flex-col space-y-4">
+    <div class="flex items-center justify-between w-full">
+        <div class="h-8 rounded bg-gray-800 w-64 mb-4">&nbsp;</div>
+        <div class="h-8 rounded bg-gray-800 w-32 mb-4">&nbsp;</div>
+    </div>
+    <div class="h-2 rounded-full bg-gray-700 max-w-md mb-2.5">&nbsp;</div>
+    <div class="h-4 rounded-full bg-gray-700 max-w-sm mb-2.5">&nbsp;</div>
+    <div class="h-3 rounded-full bg-gray-700 max-w-lg mb-2.5">&nbsp;</div>
+    <div class="h-4 rounded-full bg-gray-700 max-w-md mb-2.5">&nbsp;</div>
+    <div class="h-2 rounded-full bg-gray-700 max-w-md">&nbsp;</div>
+    <div class="h-2 rounded-full bg-gray-700 max-w-lg">&nbsp;</div>
+    <span class="sr-only">Loading...</span>
+</div>
