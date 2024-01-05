fee09e2d940832434e227d0f3611655e7bcbc321
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-05 23:44:49 +0530

    New Livewire component : `ViewProject`

--- routes/project-routes.php
+++ routes/project-routes.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Livewire\ShowProjects;
+use App\Livewire\ViewProject;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/projects', ShowProjects::class)->name('projects.index')->lazy();
+Route::get('/projects/{project}', ViewProject::class)->name('projects.view')->lazy();
