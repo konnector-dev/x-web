32f9af1f5c0452a3a04c111fb76a0b3afef679fa
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-05 23:44:10 +0530

    Cosmetic irks and quirks

--- resources/views/livewire/show-projects.blade.php
+++ resources/views/livewire/show-projects.blade.php
@@ -4,20 +4,23 @@
         <button class="p-3 px-6 rounded-lg font-semibold text-lg bg-gray-800/80" wire:click="create">New project</button>
     </div>
 
-    <ul role="list" class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 mt-6">
+    <ul role="list" class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 mt-6">
         @forelse ($projects as $project)
         <li
             wire:key="{{ $project->id }}"
-            class="col-span-1 rounded-lg bg-gray-800 shadow flex flex-col justify-between pb-4">
-            <div class="flex w-full items-center justify-between space-x-6 p-6">
+            class="group border border-gray-900 hover:border-gray-500 relative col-span-1 rounded-lg bg-gray-800 shadow flex flex-col justify-between pb-4">
+            <a class="flex w-full items-center justify-between space-x-6 p-6" href="{{ route('projects.view', $project->id) }}">
                 <div class="truncate">
-                    <div class="flex items-center space-x-3">
-                        <h3 class="font-medium">{{ $project->name }}</h3>
+                    <div class="flex items-centergroup-hover:underline">
+                        <div class="absolute top-0 right-0 p-4 text-3xl text-gray-500/30 group-hover:text-gray-200">
+                            #{{ $project->id }}
+                        </div>
+                        <h3 class="font-medium text-lg">{{ $project->name }}</h3>
                     </div>
                     <p class="mt-1 truncate hover:text-clip text-sm text-gray-500">{{ $project->description }}</p>
                 </div>
-            </div>
-            <div class="flex items-center justify-between gap-2 space-x-2 space-y-2">
+            </a>
+            <div class="flex items-center justify-between space-x-2 border border-b-0 border-l-0 border-r-0 border-gray-500/20 pt-4">
                 <div class="w-full mx-auto text-center">
                     <button class="p-2 px-4 rounded border border-gray-500 bg-gray-800/80 hover:bg-gray-700" wire:click="edit({{ $project->id }})">Edit</button>
                 </div>
