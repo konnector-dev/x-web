685a987dadd657ba85461c3b2d1cda90514d5fd4
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-05 13:47:40 +0530

    make footer uniform on all layouts

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
-    <div class="mx-auto max-w-7xl px-6 py-12 lg:flex lg:items-center lg:justify-between lg:px-8">
-        <div class="flex justify-center space-x-6 lg:order-2">
+    <div class="mx-auto max-w-7xl px-6 py-2 flex flex-col items-center justify-center lg:px-8">
+        <div class="flex justify-center space-x-6">
             <a target="_blank" href="https://twitter.com/jdecode">
                 <span class="sr-only">Twitter</span>
                 <img src="{{ asset('images/twitter-white.svg') }}" alt="Twitter" class="h-8 w-8" />
@@ -10,9 +10,9 @@
                 <img src="{{ asset('images/raw-github-white.svg') }}" alt="GitHub" class="h-8 w-8" />
             </a>
         </div>
-        <div class="mt-8 lg:order-1 lg:mt-0">
+        <div class="order-1 mt-0">
             <p class="text-center text-xs leading-5 text-gray-500">&copy; {{ date('Y') }} {{ Config::get('env_vars.APP_NAME') }} </p>
+            <x-screen-size-indicator />
         </div>
     </div>
 </footer>
-<x-screen-size-indicator />
