8a53167eb58a991109b32a9f45b567d527b7c94e
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-09 13:23:14 +0530

    Use Avatar icon package

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -28,7 +28,8 @@
 
                 <!-- Current Profile Photo -->
                 <div class="mt-2" x-show="! photoPreview">
-                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover">
+                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover hidden">
+                    <img title="{{ Auth::user()->name }}" class="rounded-full h-20 w-20 object-cover" src="{{ Avatar::create(Auth::user()->name)->toBase64() }}" alt="{{ Auth::user()->name }}" />
                 </div>
 
                 <!-- New Profile Photo Preview -->
