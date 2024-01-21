02b158ded4e80c6f1ae5f416099f78749ea0bff0
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-21 19:43:20 +0530

    Active-load, without `ping`

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
 <x-full-width-layout>
     <h3 class="text-2xl mx-4 pb-4">Dashboard</h3>
-    <livewire:urls lazy />
+    <livewire:urls />
 </x-full-width-layout>
--- resources/views/livewire/urls.blade.php
+++ resources/views/livewire/urls.blade.php
@@ -12,7 +12,7 @@
             @php
                 $color = $url->http_status_code === \Symfony\Component\HttpFoundation\Response::HTTP_OK
                         ? ' text-green-500 bg-green-400/10 '
-                        : ' text-red-500 bg-red-500/20 animate-ping animate-pulse ';
+                        : ' text-red-500 bg-red-500/20 animate-pulse ';
             @endphp
             <div class="absolute top-0 right-0 pt-4 pr-4">
                 <div class="flex-none rounded-full p-1 {{ $color }}">
