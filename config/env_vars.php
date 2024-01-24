8cd851a030a402bda303cfb16c7b047f7d1692ba
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-24 13:54:35 +0530

    ENV VAR placeholders (in config) - Token and Channel

--- config/env_vars.php
+++ config/env_vars.php
@@ -9,4 +9,7 @@ return [
     'GITHUB_CALLBACK' => env('GITHUB_CALLBACK', '/auth/github/callback'),
 
     'SENTRY_LARAVEL_DSN' => env('SENTRY_LARAVEL_DSN', 'https://dsn@oi.ingest.sentry.io/999'),
+
+    'SLACK_BOT_USER_OAUTH_TOKEN' => env('SLACK_BOT_USER_OAUTH_TOKEN', 'xoxb-1234567890-123456789012'),
+    'SLACK_BOT_USER_DEFAULT_CHANNEL' => env('SLACK_BOT_USER_DEFAULT_CHANNEL', '#watcher'),
 ];
