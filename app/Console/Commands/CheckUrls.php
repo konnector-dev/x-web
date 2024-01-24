bfec8d30d036281f6daa99a7889b30f37d190d2f
JD Singh <jagdeepsingh@founderandlightning.com>
2024-01-24 14:04:11 +0530

    Check Url > Notify Slack (if down)

--- app/Console/Commands/CheckUrls.php
+++ app/Console/Commands/CheckUrls.php
@@ -3,11 +3,13 @@
 namespace App\Console\Commands;
 
 use App\Models\Url;
+use App\Notifications\UrlIsDown;
 use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
+use Notification;
 
 class CheckUrls extends Command
 {
@@ -17,22 +19,36 @@ class CheckUrls extends Command
 
     public function handle(): void
     {
+        $this->checkUrls();
+    }
+
+    private function checkUrls(): void
+    {
         $this->info('Checking URLs...');
         $urls = Url::where('status', 'active')->get();
         foreach ($urls as $url) {
             Log::info('Checking ' . $url->url);
             try {
                 $headers = Http::get($url->url);
-                $url->http_status_code = $headers->successful() ? Response::HTTP_OK : Response::HTTP_INTERNAL_SERVER_ERROR;
+                $url->http_status_code = $headers->successful(
+                ) ? Response::HTTP_OK : Response::HTTP_INTERNAL_SERVER_ERROR;
                 $url->state = $headers->successful() ? Url::STATE_UP : Url::STATE_DOWN;
             } catch (Exception $e) {
                 report($e);
                 $url->http_status_code = Response::HTTP_INTERNAL_SERVER_ERROR;
                 $url->state = Url::STATE_ERROR;
                 Log::error("$url->http_status_code: $url->url");
+                $this->notifySlack($url);
             }
             $url->last_checked_at = now()->format('Y-m-d H:i:s');
             $url->save();
         }
+    }
+
+    private function notifySlack(Url $url): void
+    {
+        $this->info('Sending Slack notification...');
+        Notification::route('slack', config('env_vars.SLACK_BOT_USER_DEFAULT_CHANNEL'))
+            ->notify(new UrlIsDown($url));
     }
 }
