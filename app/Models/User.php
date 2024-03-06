14c006b0567c3f99099bb0b279b9b9973b6b2f92
Jagdeep Singh Kalsi <jdecode@gmail.com>
2024-03-06 11:34:46 +0530

    Remove unused import

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
