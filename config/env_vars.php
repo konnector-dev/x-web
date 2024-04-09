6e3933570f6740b3ced91f108c96dfc3581cffb7
JD Singh <jagdeepsingh@founderandlightning.com>
2024-04-09 23:21:20 +0530

    x-web > axe-web (docker extensions conflict)

--- config/env_vars.php
+++ config/env_vars.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Str;
 
 return [
-    'APP_NAME' => env('APP_NAME', 'x-web'),
+    'APP_NAME' => env('APP_NAME', 'axe-web'),
     'CACHE_PREFIX' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', '_x_web_'), '_') . '__'),
 ];
