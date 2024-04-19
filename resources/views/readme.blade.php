38d9eb32a51f36905426ac222f9bea3e650c47cb
JD Singh <jagdeepsingh@founderandlightning.com>
2024-04-19 11:52:59 +0530

    Add MD based layout for readme

---
+++ resources/views/readme.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png" />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    </head>
+    <body class="font-sans antialiased text-black/90 dark:text-white/90 backdrop-blur-2xl"
+          style="background: url('{{ asset("assets/images/tapet-05.jpg") }}')">
+        <div
+            class="bg-fixed bg-center bg-white/30 dark:bg-black/70"
+        >
+            <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-blue-500 selection:text-white">
+                <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                        <div class="flex lg:justify-center lg:col-start-2">
+                            <x-application-logo />
+                        </div>
+                        @if (Route::has('login'))
+                            <nav class="-mx-3 flex flex-1 justify-end">
+                                @auth
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#3B82F6] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Dashboard
+                                    </a>
+                                @else
+                                    <a
+                                        href="{{ route('login') }}"
+                                        class="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#3B82F6] dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Log in
+                                    </a>
+
+                                    @if (Route::has('register'))
+                                        <a
+                                            href="{{ route('register') }}"
+                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#3B82F6] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            Register
+                                        </a>
+                                    @endif
+                                @endauth
+                            </nav>
+                        @endif
+                    </header>
+
+                    <main class="mt-6">
+                        <div class="">
+                            <div class="flex items-start rounded-lg bg-white dark:bg-black text-black dark:text-white p-6">
+                                <div class="pt-3 sm:pt-5 prose">
+                                    <h2 class="text-xl font-semibold">README.md</h2>
+
+                                    <p class="mt-4 text-sm/relaxed">
+                                        {!! $readme !!}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+
+                    <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
